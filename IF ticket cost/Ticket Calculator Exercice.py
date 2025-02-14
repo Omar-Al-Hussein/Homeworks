654e03b3061b779b6ff876303672fadf68c5f3a9
Omar-Al-Hussein <omariz201184@gmail.com>
2025-02-14 16:51:54 +0200

    Ticket Calculator Implemented

---
+++ IF ticket cost/Ticket Calculator Exercice.py
@@ -0,0 +1,10 @@
+age = int(input('Enter your age in years: '))
+
+if age <= 18:
+ print('the ticket is 2$')
+elif age >= 18:
+ print("the ticket is 5$")
+elif age >= 30: 
+ print('the ticket is 10$')
+else:
+ print('the ticket is 5$')
